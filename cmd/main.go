63cda121cf7f319f7a9fb566648287bc47b1ae4e
Corentin Bruneau <ccbbruneau@gmail.com>
2018-03-24 16:55:41 +0100

    Add interpolators & polylinear gradients

--- cmd/main.go
+++ cmd/main.go
@@ -3,73 +3,94 @@ package main
 import (
 	"fmt"
 	"github.com/dancing-koala/gradient/pkg/hexcolor"
+	"github.com/dancing-koala/gradient/pkg/interpolator"
 	"image"
 	"image/color"
 	"image/png"
 	"os"
 )
 
+type step struct {
+	start int
+	end   int
+}
+
 func main() {
 
 	left, top := 0, 0
-	right, bottom := 800, 200
+	width, height := 800, 200
 
-	startHex := "#00000000"
-	endHex := "#FF000000"
-
-	startColor, err := hexcolor.ParseToRGBA(startHex)
-
-	if err != nil {
-		fmt.Printf("Could not parse start color<%s>: %s\n", startHex, err)
-		return
+	hexColors := []string{
+		"#00FF00",
+		"#FF0000",
+		// "#00FF00",
+		// "#FFFF00",
+		// "#FF0000",
+		// "#FFFF00",
+		// "#00FF00",
+		// "#00FFFF",
+		// "#0000FF",
 	}
 
-	endColor, err := hexcolor.ParseToRGBA(endHex)
+	steps := make([]step, len(hexColors)-1)
 
-	if err != nil {
-		fmt.Printf("Could not parse end color<%s>: %s\n", startHex, err)
-		return
-	}
+	createSteps(steps, width)
 
-	img := image.NewRGBA(image.Rect(left, top, right, bottom))
+	offset := 0
 
-	r, g, b, a := startColor.RGBA()
-	rr, gg, bb, aa := endColor.RGBA()
+	img := image.NewRGBA(image.Rect(left, top, width, height))
 
-	fa := float64(a & 0xFF)
-	fr := float64(r & 0xFF)
-	fg := float64(g & 0xFF)
-	fb := float64(b & 0xFF)
+	var startColor, endColor color.Color
+	var err error
 
-	astep := (float64(aa&0xFF) - fa) / float64(right)
-	rstep := (float64(rr&0xFF) - fr) / float64(right)
-	gstep := (float64(gg&0xFF) - fg) / float64(right)
-	bstep := (float64(bb&0xFF) - fb) / float64(right)
+	for _, currStep := range steps {
+		startColor, err = hexcolor.ParseToRGBA(hexColors[offset])
 
-	var newa, newr, newg, newb uint8
-
-	for i := 0; i < right; i++ {
-
-		newa = uint8(fa)
-		newr = uint8(fr)
-		newg = uint8(fg)
-		newb = uint8(fb)
-
-		c := &color.RGBA{
-			A: newa,
-			R: newr,
-			G: newg,
-			B: newb,
+		if err != nil {
+			fmt.Printf("Could not parse start color<%s>: %s\n", hexColors[offset], err)
+			return
 		}
 
-		for j := 0; j < bottom; j++ {
-			img.Set(i, j, c)
+		endColor, err = hexcolor.ParseToRGBA(hexColors[offset+1])
+
+		if err != nil {
+			fmt.Printf("Could not parse end color<%s>: %s\n", hexColors[offset+1], err)
+			return
 		}
 
-		fa += astep
-		fr += rstep
-		fg += gstep
-		fb += bstep
+		r, g, b, a := startColor.RGBA()
+		rr, gg, bb, aa := endColor.RGBA()
+
+		rInterpol := interpolator.NewLinearInterpolator(float64(r&0xFF), float64(rr&0xFF))
+		gInterpol := interpolator.NewLinearInterpolator(float64(g&0xFF), float64(gg&0xFF))
+		bInterpol := interpolator.NewLinearInterpolator(float64(b&0xFF), float64(bb&0xFF))
+		aInterpol := interpolator.NewLinearInterpolator(float64(a&0xFF), float64(aa&0xFF))
+
+		var newa, newr, newg, newb uint8
+		var progress float64
+
+		for i := currStep.start; i < currStep.end; i++ {
+
+			progress = float64(i-currStep.start) / float64(currStep.end-currStep.start)
+
+			newr = uint8(rInterpol.Interpolate(progress))
+			newg = uint8(gInterpol.Interpolate(progress))
+			newb = uint8(bInterpol.Interpolate(progress))
+			newa = uint8(aInterpol.Interpolate(progress))
+
+			c := &color.RGBA{
+				R: newr,
+				G: newg,
+				B: newb,
+				A: newa,
+			}
+
+			for j := 0; j < height; j++ {
+				img.Set(i, j, c)
+			}
+		}
+
+		offset++
 	}
 
 	fmt.Println("Saving image to output file...")
@@ -80,7 +101,30 @@ func main() {
 
 	if err != nil {
 		fmt.Println("Error while writing generated image: ", err)
+		return
 	}
 
 	fmt.Println("Done !!!")
+}
+
+func createSteps(steps []step, size int) {
+	gapSize := size / len(steps)
+
+	steps[0] = step{
+		start: 0,
+		end:   gapSize,
+	}
+
+	if gapSize != size {
+		for i := 1; i < len(steps); i++ {
+			steps[i] = step{
+				start: steps[i-1].start + gapSize,
+				end:   steps[i-1].end + gapSize,
+			}
+		}
+	}
+
+	if steps[len(steps)-1].end < size {
+		steps[len(steps)-1].end = size
+	}
 }
