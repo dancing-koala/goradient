5ede58d036fe988494971ff5732c581dd3a14389
Corentin Bruneau <c.bruneau@ubitransports.com>
2018-03-25 14:46:59 +0200

    Improve linear interpolation, add quadratic & cubic interpolators

--- pkg/interpolator/interpolator.go
+++ pkg/interpolator/interpolator.go
@@ -5,20 +5,66 @@ type Interpolator interface {
 }
 
 type linearInterpolator struct {
-	src  float64
-	diff float64
+	src float64
+	dst float64
 }
 
-func (l *linearInterpolator) Interpolate(input float64) float64 {
-	input = clamp(input, 0.0, 1.0)
-
-	return l.src + l.diff*input
+func (l *linearInterpolator) Interpolate(t float64) float64 {
+	t = clamp(t, 0.0, 1.0)
+	return (1.0-t)*l.src + t*l.dst
 }
 
-func NewLinearInterpolator(src, dst float64) *linearInterpolator {
+func NewLinear(src, dst float64) *linearInterpolator {
 	return &linearInterpolator{
-		src:  src,
-		diff: dst - src,
+		src: src,
+		dst: dst,
+	}
+}
+
+type quadraticInterpolator struct {
+	first  float64
+	second float64
+	third  float64
+}
+
+func (q *quadraticInterpolator) Interpolate(t float64) float64 {
+	t = clamp(t, 0.0, 1.0)
+
+	oneMinusT := 1.0 - t
+
+	return oneMinusT*(oneMinusT*q.first+2*t*q.second) + t*t*q.third
+}
+
+func NewQuadratic(first, second, third float64) *quadraticInterpolator {
+	return &quadraticInterpolator{
+		first:  first,
+		second: second,
+		third:  third,
+	}
+}
+
+type cubicInterpolator struct {
+	first  float64
+	second float64
+	third  float64
+	fourth float64
+}
+
+func (c *cubicInterpolator) Interpolate(t float64) float64 {
+	t = clamp(t, 0.0, 1.0)
+	tSqr := t * t
+	oneMinusT := 1 - t
+	oneMinusTSqr := oneMinusT * oneMinusT
+
+	return oneMinusTSqr*(oneMinusT*c.first+t*c.second) + tSqr*(oneMinusT*c.third+t*c.fourth)
+}
+
+func NewCubic(first, second, third, fourth float64) *cubicInterpolator {
+	return &cubicInterpolator{
+		first:  first,
+		second: second,
+		third:  third,
+		fourth: fourth,
 	}
 }
 
